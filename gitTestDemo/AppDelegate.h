c37808bad8c621e1562bb18dde9e7a15385089be
yayoyo <532128562@qq.com>
2017-09-20 15:23:02 +0800

    Initial Commit

---
+++ gitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTestDemo
+//
+//  Created by Apple on 2017/9/20.
+//  Copyright © 2017年 tiantuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTestDemo
+//
+//  Created by Apple on 2017/9/20.
+//  Copyright © 2017年 tiantuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
